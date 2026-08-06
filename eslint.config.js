115112eaf8f30f48fac34cbe6351508646c688f2
Escher Wright-Dykhouse <escher@gencitylabs.com>
2026-08-06 16:09:53 -0700

    Add linting, remove unnecessary inertia files, add tailwind as module

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,5 @@
+import prettier from 'eslint-config-prettier'
+import svelte from 'eslint-plugin-svelte'
 import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+
+export default configApp(prettier, svelte.configs.prettier)
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import inertia from '@adonisjs/inertia/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import adonisjs from '@adonisjs/vite/client'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     inertia({ ssr: { enabled: true, entrypoint: 'inertia/ssr.tsx' } }),
     svelte(),
     adonisjs({ entrypoints: ['inertia/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    tailwind()
   ],
 
   /**
